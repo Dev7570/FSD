a77b736f36f479e0066cf995368554acbdea8cf3
Dev7570 <devggre@gmail.com>
2025-09-17 14:28:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+//function
+console.log(fun);
+//nsole.log(fun());
+function fun()
+{
+    console.log(name);
+  var name = "Anshika";
+     name = "Gupta";
+    console.log(name);
+    return name;
+}
+console.log(fun() , "from invocation");
